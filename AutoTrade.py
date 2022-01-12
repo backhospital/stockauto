2d82e995be2f05400fecd367aab5323a1dfba53a
backhospital <87355666+backhospital@users.noreply.github.com>
2022-01-12 11:20:43 +0900

    Update AutoTrade.py

--- AutoTrade.py
+++ AutoTrade.py
@@ -14,7 +14,7 @@ def post_message(token, channel, text):
         data={"channel": channel,"text": text}
     )
  
-myToken = "xoxb-2952779607568-2922389014598-FW2jEwGVJmMnQgvKFQuukndp"
+myToken = "xoxb-**** slack쿠폰번호"
 def dbgout(message):
     """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
